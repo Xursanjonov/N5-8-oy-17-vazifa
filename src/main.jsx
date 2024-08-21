2d08b575a93f05645a336fcae1051be67c6c8d83
NusratilloDev <khursanjonov@gmail.com>
2024-08-21 07:35:42 +0500

    brr

--- src/main.jsx
+++ src/main.jsx
@@ -1,17 +1,21 @@
-import React, { Suspense } from 'react'
+import React, { lazy, Suspense } from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
 import Loader from './components/loader/index.jsx'
 import { BrowserRouter } from 'react-router-dom'
+import { Provider } from 'react-redux'
 import 'number-brm'
+import store from './context/index.js'
 import './index.css'
+const App = lazy(() => import('./App.jsx'))
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   // <React.StrictMode>
   <Suspense fallback={<Loader />}>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <Provider store={store}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Provider>
   </Suspense>
   // </React.StrictMode>,
 )
---
+++ src/pages/admin/manage/ManageProduct.jsx
@@ -0,0 +1,14 @@
+import React, { memo } from 'react'
+
+const ManageProduct = () => {
+
+    return (
+        <div className='w-full'>
+            <h1 className='text-3xl font-bold'>Manage Product</h1>
+            <div className="w-full flex items-center justify-start gap-4">
+            </div>
+        </div>
+    )
+}
+
+export default memo(ManageProduct)
